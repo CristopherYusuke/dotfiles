142b07b68eb688ab77e4db8c37dc0599c156b869
Cristopher Martins <cristopher.yusuke@gmail.com>
2026-04-25 20:13:43 +0100

    initial commit

---
+++ nvim/lua/plugins/colortheme.lua
@@ -0,0 +1,59 @@
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		local opt = {
+			flavour = "mocha",
+			background = {
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = true,
+			show_end_of_buffer = false,
+			term_colors = true,
+			dim_inactive = {
+				enabled = false,
+				shade = "dark",
+				percentage = 0.15,
+			},
+			styles = {
+				comments = { "italic" },
+				conditionals = { "italic" },
+				keywords = { "bold" },
+				functions = {},
+				variables = {},
+			},
+			integrations = {
+				bufferline = true,
+				cmp = true,
+				gitsigns = true,
+				neotree = true,
+				telescope = { enabled = true },
+				treesitter = true,
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+					},
+					inlay_hints = {
+						background = true,
+					},
+				},
+			},
+		}
+
+		require("catppuccin").setup(opt)
+		vim.cmd.colorscheme("catppuccin-nvim")
+	end,
+}
