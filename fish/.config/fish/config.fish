d920e9d91ccab8e2b3f23f213689434eb3840d2f
Cristopher Martins <cristopher.yusuke@gmail.com>
2026-04-27 00:37:29 +0100

    cedilha and intl layout

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,12 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
 #    # smth smth
 #end
+
+set -gx GTK_IM_MODULE cedilla
+set -gx QT_IM_MODULE cedilla
 
 starship init fish | source 
 
