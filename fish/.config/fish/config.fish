ddf1d9d15a0fe0af4f901414c8aa62b0bd2e2531
Cristopher Martins <cristopher.yusuke@gmail.com>
2026-05-14 19:28:07 +0100

    fix cedilha with alt-intl variant and some env

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,17 +1,22 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+
+# Greeting
+# function fish_greeting
+#     fastfetch
+# end
+#
+fish_vi_key_bindings
 
 set -gx GTK_IM_MODULE cedilla
 set -gx QT_IM_MODULE cedilla
 
-
-
 if status is-interactive
+    # Input methods
+
+    # Editor
+    set -gx EDITOR nvim
+
+    # Tools
     fzf --fish | source
     starship init fish | source
-    # outros comandos interativos aqui
 end
